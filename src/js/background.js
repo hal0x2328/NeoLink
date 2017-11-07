d20b7233a55ffce89d57110ad741d0b0a7ad2baf
phetter <fet@fet>
2017-11-07 16:09:32 -0500

    tests

--- src/js/background.js
+++ src/js/background.js
@@ -1,2 +1,18 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
+
+
+console.log("in background")
+
+
+// chrome.browserAction.onClicked.addListener(function() {
+  // Fired when a browser action icon is clicked. This event will not fire if the browser action has a popup.
+//   console.log('browser action!')
+//
+// });
+
+var foo = 'test';
+
+// function foo() {
+//  console.log("I'm defined in background.js");
+// }
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,32 +1,63 @@
-import "../css/popup.css";
+// var bgpage = chrome.extension.getBackgroundPage();
+//
+// import util from "util"
+//
+// // console.log('bgpage: '+util.inspect(bgpage, {depth: null}))
+//
+// if(bgpage) {
+//   console.log('foo:'+bgpage.foo);
+//   // bgpage.foo());
+// }
+// else console.log('bgpage notfound')
 
-//import neon from "neon-js"
-import * as neon from 'neon-js'
-var address='AJXixUHZZsSZTrrP7ZpRqKbY2HzRawf8cB';
+chrome.extension.getBackgroundPage(function (backgroundPage) {
+    console.log(backgroundPage.foo); // Displays "mooh".
+});
 
-neon.getTransactionHistory('MainNet', address)
-  .then((transactions) => {
-    let txs = [];
-    for (let i = 0; i < transactions.length; i++){
-      if (transactions[i].neo_sent === true){
-        console.log("NEO: " + transactions[i].txid);
-      }
-      if (transactions[i].gas_sent === true){
-        console.log("GAS: " + transactions[i].txid);
-      }
-    }}).catch((e) => {
-        console.log(e)
-        throw e
-      })
-
-neon.getBalance('MainNet', address)
-      .then((response) => {
-        document.getElementById("content").innerHTML = "<h3>" +
-		address + "</h3><br>NEO Balance: " + response.NEO.balance +
-		"<br>GAS: " + response.GAS.balance;
-	console.log(address +"\nNEO Balance: " + response.NEO.balance + "\nGas Balance: " + response.GAS.balance);
-      }).catch((e) => {
-        console.log(e)
-        throw e
-      })
+// import "../css/popup.css";
+//
+// //import neon from "neon-js"
+// import * as neon from 'neon-js'
+// var address='AJXixUHZZsSZTrrP7ZpRqKbY2HzRawf8cB';
+//
+// neon.getTransactionHistory('MainNet', address)
+//   .then((transactions) => {
+//     let txs = [];
+//     for (let i = 0; i < transactions.length; i++){
+//       if (transactions[i].neo_sent === true){
+//         console.log("NEO: " + transactions[i].txid);
+//       }
+//       if (transactions[i].gas_sent === true){
+//         console.log("GAS: " + transactions[i].txid);
+//       }
+//     }}).catch((e) => {
+//         console.log(e)
+//         throw e
+//       })
+//
+// neon.getBalance('MainNet', address)
+//       .then((response) => {
+//         document.getElementById("content").innerHTML = "<h3>" +
+// 		address + "</h3><br>NEO Balance: " + response.NEO.balance +
+// 		"<br>GAS: " + response.GAS.balance;
+// 	console.log(address +"\nNEO Balance: " + response.NEO.balance + "\nGas Balance: " + response.GAS.balance);
+//       }).catch((e) => {
+//         console.log(e)
+//         throw e
+//       })
 //hello();
+
+// var bgWin = chrome.extension.getBackgroundPage();
+// console.log(bgWin.backgroundFunction());
+
+// function onGot(page) {
+//   page.foo();
+// }
+//
+// function onError(error) {
+//   console.log(`Error: ${error}`);
+// }
+//
+// var getting = chrome.extension.getBackgroundPage();
+// // getting.then(onGot, onError);
+// getting.foo();
